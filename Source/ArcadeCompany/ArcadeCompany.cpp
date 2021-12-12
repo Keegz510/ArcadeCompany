84acde66c24d4ece01f0033abdefc6318e5e03d8
Keegz510 <s200322@students.aie.edu.au>
2021-12-12 21:32:52 +1100

    Initial Commit

---
+++ Source/ArcadeCompany/ArcadeCompany.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArcadeCompany.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArcadeCompany, "ArcadeCompany" );
