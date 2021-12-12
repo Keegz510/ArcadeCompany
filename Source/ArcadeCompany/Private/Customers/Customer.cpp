e38863b1e07ee247737d7e9db840d228192ce01c
Keegz510 <s200322@students.aie.edu.au>
2021-12-13 09:01:09 +1100

    Removed Default Methods

--- Source/ArcadeCompany/Private/Customers/Customer.cpp
+++ Source/ArcadeCompany/Private/Customers/Customer.cpp
@@ -1,13 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Customers/Customer.h"
+#include "ArcadeCompany/Public/Customers/Customer.h"
 
 // Sets default values
 ACustomer::ACustomer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -18,17 +18,4 @@ void ACustomer::BeginPlay()
 	
 }
 
-// Called every frame
-void ACustomer::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void ACustomer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
