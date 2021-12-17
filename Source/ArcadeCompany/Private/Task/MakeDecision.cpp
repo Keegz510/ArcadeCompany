ef380a2f4c4ee15c4e3f5640b86aa592703b8f86
Keegz510 <s200322@students.aie.edu.au>
2021-12-17 14:15:27 +1100

    Created a task to help the AI make a decision on thenext task

--- Source/ArcadeCompany/Private/Task/MakeDecision.cpp
+++ Source/ArcadeCompany/Private/Task/MakeDecision.cpp
@@ -3,3 +3,44 @@
 
 #include "Task/MakeDecision.h"
 
+#include "Customers/CustomerController.h"
+#include "Customers/Customer.h"
+
+EBTNodeResult::Type UMakeDecision::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+	if(auto controller = Cast<ACustomerController>(OwnerComp.GetAIOwner()))
+	{
+		if(auto customer = Cast<ACustomer>(OwnerComp.GetOwner()))
+		{
+			int32 tokens = customer->GetTokensAmount();
+			if(tokens == 0)
+			{
+				int32 randValue = FMath::RandRange(1, 100);
+				if(randValue > 70)
+				{
+					controller->UpdateCustomerState(ECustomerState::Leaving);
+				} else
+				{
+					controller->UpdateCustomerState(ECustomerState::BuyingTokens);
+				}
+			} else
+			{
+				int randValue = FMath::RandRange(1, 100);
+				if(randValue < 50)
+				{
+					controller->UpdateCustomerState(ECustomerState::FindMachine);
+				} else if (randValue > 50 && randValue < 75)
+				{
+					controller->UpdateCustomerState(ECustomerState::WantsSnack);
+				} else
+				{
+					controller->UpdateCustomerState(ECustomerState::Leaving);
+				}
+			}
+		}
+	}
+
+
+	return EBTNodeResult::Failed;
+}
