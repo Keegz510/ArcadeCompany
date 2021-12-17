bda94c368121191471a16006da54f25b042925c8
Keegz510 <s200322@students.aie.edu.au>
2021-12-17 16:07:28 +1100

    Updating

---
+++ Source/ArcadeCompany/Private/Task/BuyingTokens.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Task/BuyingTokens.h"
+
+#include "Customers/Customer.h"
+#include "Customers/CustomerController.h"
+
+EBTNodeResult::Type UBuyingTokens::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+	if(auto controller = Cast<ACustomerController>(OwnerComp.GetAIOwner()))
+	{
+		controller->UpdateCustomerState(ECustomerState::WaitingForTokens);
+		return EBTNodeResult::Succeeded;
+	}
+
+	return EBTNodeResult::Failed;
+}
